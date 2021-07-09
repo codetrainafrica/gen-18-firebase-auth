71c278f225eed487f80a461ca114d07cde588375
prince antwi <antwi2k@gmail.com>
2021-07-09 15:44:10 +0800

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container,Row,Col } from 'react-bootstrap';
+import UserForm from "./Component/UserForm";
+import AddUser from "./Component/AddUser";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props){
+      super(props)
+      this.state= {
+        users: [
+          {
+            name:" Prince Antwi",
+            contact: "0572211378",
+            location: "lapaz"
+          }
+        ]
+      }
+    }
+    handleAddUser = (user) => {
+      this.setState({ 
+        users: [...this.state.users, user ]});
+    };
+   
+  render() {
+    return (
+      <>
+      <Container>
+        <Row>
+          <Col xs={6} md={4}>
+            <AddUser adduser={this.handleAddUser} />
+          </Col>
+          <Col xs={6} md={8}>
+            <UserForm  userDate={this.state.users}/>
+          </Col>
+        </Row>
+      </Container>
+      </>
+    );
+  }
 }
 
 export default App;
+
