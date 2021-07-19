61d98e63063080313bfb16f5d16099348347656e
prince antwi <antwi2k@gmail.com>
2021-07-19 14:13:08 +0800

    added UserForm

--- src/Component/UserForm.jsx
+++ src/Component/UserForm.jsx
@@ -4,8 +4,15 @@ import User from "./User";
 const Users = (props) => {
   return (
     <div>
-      {props.userDate.map((user, index) => {
-        return <User userinfo={user} key={index} />;
+      {props.userDate.map((user) => {
+        return (
+          <User
+            userinfo={user}
+            key={user.id}
+            deleteuser={props.deleteuser}
+            eidthuser={props.eidthuser}
+          />
+        );
       })}
     </div>
   );
