619746136df37819f9ece1b4a44871229d871673
princeantwi2k <antwi2k@gmail.com>
2021-08-18 16:30:16 +0800

    added UserForm

--- src/Component/UserForm.jsx
+++ src/Component/UserForm.jsx
@@ -1,21 +1,42 @@
 import React from "react";
-import User from "./User";
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { connect } from "react-redux";
 
-const Users = (props) => {
+const Users = ({ users }) => {
   return (
     <div>
-      {props.userDate.map((user) => {
+      {users.map((user, idex) => {
         return (
-          <User
-            userinfo={user}
-            key={user.id}
-            deleteuser={props.deleteuser}
-            eidthuser={props.eidthuser}
-          />
+          <div key={idex}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Super card
+                </Card.Subtitle>
+                <Card.Title>Name: {user.name}</Card.Title>
+                <Card.Text>
+                  <p>Contact: {user.contact}</p>
+                  <p>Location: {user.location}</p>
+                </Card.Text>
+                <Button variant="outline-primary" size="sm">
+                  Eidth
+                </Button>{" "}
+                <Button variant="outline-dark" size="sm">
+                  Delete
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
         );
       })}
     </div>
   );
 };
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+  };
+};
 
-export default Users;
+export default connect(mapStateToProps)(Users);
