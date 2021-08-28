84ee079144da14ce1c54e27281ff3c2a86530aba
princeantwi2k <antwi2k@gmail.com>
2021-08-28 12:25:20 +0800

    added User.jsx

--- src/Component/User.jsx
+++ src/Component/User.jsx
@@ -1,34 +1,55 @@
 import React, { useState } from "react";
-
 import { Modal } from "react-bootstrap";
-import EidthUser from "./EidthUser";
+import { connect } from "react-redux";
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import EditUser from "./EditUser";
+import { deleteUser } from "../AddAction/AddAction";
 
-const User = (props) => {
+const User = ({ users, deleteUser, editUser }) => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const handleDelete = (e) => {
     e.preventDefault();
-    props.deleteuser(props.userinfo.id);
+    deleteUser(users.id);
   };
-
   return (
     <>
       <Modal show={show} onHide={handleClose} animation={false}>
-        <Modal.Header>
-          <Modal.Title>EidthUser</Modal.Title>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Contact</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <EidthUser
-            userinfo={props.userinfo}
-            eidthuser={props.eidthuser}
+          <EditUser
+            users={users}
+            editUser={editUser}
             closeModal={handleClose}
           />
         </Modal.Body>
       </Modal>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Subtitle className="mb-2 text-muted">Super card</Card.Subtitle>
+          <Card.Title>Name:{users.name}</Card.Title>
+          <Card.Text>
+            <p>Contact: {users.contact}</p>
+            <p>Location: {users.location}</p>
+          </Card.Text>
+          <Button variant="outline-primary" size="sm" onClick={handleShow}>
+            Eidth
+          </Button>{" "}
+          <Button variant="outline-dark" size="sm" onClick={handleDelete}>
+            Delete
+          </Button>
+        </Card.Body>
+      </Card>
     </>
   );
 };
+const mapDispatchToProps = {
+  deleteUser: deleteUser,
+};
 
-export default User;
+export default connect(null, mapDispatchToProps)(User);
