f02f96e2531a5ee60331a58fae16dc7687264247
princeantwi2k <antwi2k@gmail.com>
2021-08-28 12:25:45 +0800

    added Userlist.jsx

---
+++ src/Component/Userlist.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { connect } from "react-redux";
+
+import User from "./User";
+
+const Userlist = ({ users, deleteUser, editUser }) => {
+  const contactlist = users.map((users) => {
+    return <User users={users} deleteUser={deleteUser} editUser={editUser} />;
+  });
+  return <div>{contactlist}</div>;
+};
+
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+  };
+};
+
+export default connect(mapStateToProps)(Userlist);
